e5edec1b9b3a51e7097945c2cb5d38f7f67c401a
lluanR <lluan.rodrigues20@gmail.com>
2025-01-30 22:25:13 -0300

    Tipos Primitivos

--- src/fundamentos/TiposPrimitivos.java
+++ src/fundamentos/TiposPrimitivos.java
@@ -2,5 +2,20 @@ package fundamentos;
 
 public class TiposPrimitivos {
 
-    /* Sao no total de 8 os tipos primitivos utilizados
+    // byte -128 até 127;
+    byte minByte = -128;
+    byte maxByte = 127;
+
+    // short = -32.768 até 32.767;
+    short minShort = -32_768;
+    short maxShort = 32_767;
+
+    // int = -2.147.483.648 até 2.147.483.647;
+    int minInt = -2_147_483_648;
+    int maxInt = 2_147_483_647;
+
+    // long = -9.223.372.036.854.775.808 até 9.223.372.036.854.775.807;
+    long minLong = -9_223_372_036_854_775_808L;
+    long maxLong = 9_223_372_036_854_775_807L;
+
 }
